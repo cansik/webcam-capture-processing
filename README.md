d9222dd3f09823726cc2d74504e229b0a651ff2f
Florian Bruggisser <cansik@users.noreply.github.com>
2021-02-13 19:41:47 +0100

    implemented basic capture device to read images

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# webcam-capture-processing
+# Webcam Capture Processing
 A very basic webcam capture solution for processing.
+
+```processing
+Capture cam;
+
+public void setup() {
+    size(640, 480);
+
+    cam = new Capture(this, 640, 480);
+    cam.start();
+}
+
+public void draw() {
+    background(55);
+
+    if (cam.available()) {
+        cam.read();
+    }
+
+    image(cam, 0, 0, width, height);
+}
+```
--- src/main/java/ch/bildspur/video/Capture.java
+++ src/main/java/ch/bildspur/video/Capture.java
@@ -1,10 +1,15 @@
 package ch.bildspur.video;
 
+import com.github.sarxos.webcam.Webcam;
 import processing.core.PApplet;
 import processing.core.PImage;
 
+import java.awt.*;
+import java.nio.ByteBuffer;
+
 public class Capture extends PImage {
-    private PApplet sketch;
+    private final PApplet sketch;
+    private Webcam webcam;
 
     public Capture(PApplet sketch, int width, int height) {
         super(width, height, RGB);
@@ -13,18 +18,28 @@ public class Capture extends PImage {
     }
 
     public void start() {
-
+        webcam = Webcam.getDefault();
+        webcam.setViewSize(new Dimension(width, height));
+        webcam.open();
     }
 
     public void read() {
+        ByteBuffer data = webcam.getImageBytes();
 
+        for(int i = 0; i < pixels.length; i++) {
+            pixels[i] = (data.get() & 0xFF) << 16 |
+                        (data.get() & 0xFF) << 8 |
+                        (data.get() & 0xFF);
+        }
+
+        updatePixels();
     }
 
     public void stop() {
-
+        webcam.close();
     }
 
     public boolean available() {
-        return false;
+        return webcam.isImageNew();
     }
 }
